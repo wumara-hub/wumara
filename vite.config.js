524d1878d603bbe707c673ba3a98d66a86a02541
Colin <colinmaraschin@gmail.com>
2026-04-12 21:59:08 -0700

    fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     open: true,
     host: true
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        photography: resolve(__dirname, 'photography.html'),
+        videos: resolve(__dirname, 'videos.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        secret: resolve(__dirname, 'secret.html'),
+      }
+    }
+  },
+  publicDir: 'public'
 })
