223a211fb1f32683873ddf8b4daf159e745a39b0
Colin <colinmaraschin@gmail.com>
2026-04-18 16:43:29 -0700

    add new pages to vite build

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         photography: resolve(__dirname, 'photography.html'),
         videos: resolve(__dirname, 'videos.html'),
+        collageCore: resolve(__dirname, 'collage-core.html'),
+        video: resolve(__dirname, 'video.html'),
         projects: resolve(__dirname, 'projects.html'),
         secret: resolve(__dirname, 'secret.html'),
       }
